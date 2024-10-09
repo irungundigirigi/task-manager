0b9ee5bb9a7fe6acb7a86796c9a736d3abdce45e
irungundigirigi <irungundigirigi@gmail.com>
2024-10-09 16:40:35 +0300

    useeffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react'
+import { Task } from './types/Task';
 import './App.css'
 
 function App() {
 
+  const [data, setData] = useState({});
+
+  useEffect (() => {
+
+    const fetchData = async() => {
+      const response = await fetch(`http://localhost:8080/api/v1/tasks`);
+      const data = await response.json();
+      setData(data.content)
+    }
+    fetchData()
+  },[])
+
   return (
     <>
+    <ul>
+    {data.map((task) => {
+      <li key={task.id}>
+        {task.subject}
+        {task.description}
+
+      </li>
+    })}
+    </ul>
     </>
   )
 }
