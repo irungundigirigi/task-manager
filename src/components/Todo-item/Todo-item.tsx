553d8fc5f55ce431313f68d6c8f13ffce461746b
irungundigirigi <irungundigirigi@gmail.com>
2024-10-11 15:22:56 +0300

    add priority

--- src/components/Todo-item/Todo-item.tsx
+++ src/components/Todo-item/Todo-item.tsx
@@ -4,12 +4,13 @@ import "./task-item.css";
 function TaskCard(props) {
   return (
     <div className="task">
-      <div className="subject">
-        {" "}
-        {props.task.task_priority} {props.task.status_id}
+      <div className="topbar">
+        <div className="bar">{props.task.task_priority} </div>
+        <div>{props.task.status_id}</div>
       </div>
       <div className="subject"> {props.task.subject}</div>
       <div className="description"> {props.task.description}</div>
+      <div className="duedate">Due: {props.task.due_date}</div>
     </div>
   );
 }
