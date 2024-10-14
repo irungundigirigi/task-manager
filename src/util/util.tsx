da10486fbc4d2f3c78ab1419c12de9848821fbc1
irungundigirigi <irungundigirigi@gmail.com>
2024-10-14 16:01:55 +0300

    prettier format

--- src/util/util.tsx
+++ src/util/util.tsx
@@ -1,14 +1,16 @@
 import { Task } from "../types/Task";
 
 export function calculateRemainingHours(task: Task): string {
-    const now = new Date();
-    const dueDate = new Date(task.due_date);
+  const now = new Date();
+  const dueDate = new Date(task.due_date);
 
-    const differenceInMilliseconds = dueDate.getTime() - now.getTime();
-    const differenceInHours = Math.max(0, Math.ceil(differenceInMilliseconds / (1000 * 60 * 60))); // Use Math.max to avoid negative hours
-    const days = Math.floor(differenceInHours / 24);
-    const hours = differenceInHours % 24;
+  const differenceInMilliseconds = dueDate.getTime() - now.getTime();
+  const differenceInHours = Math.max(
+    0,
+    Math.ceil(differenceInMilliseconds / (1000 * 60 * 60))
+  );
+  const days = Math.floor(differenceInHours / 24);
+  const hours = differenceInHours % 24;
 
-    return `${days} days ${hours} hours`;
+  return `${days} days ${hours} hours`;
 }
-
