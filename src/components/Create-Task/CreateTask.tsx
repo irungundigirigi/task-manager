12a449f3b1ae8f3fe2f878c247936deed091b3da
irungundigirigi <irungundigirigi@gmail.com>
2024-10-22 16:17:01 +0300

    create task form

---
+++ src/components/Create-Task/CreateTask.tsx
@@ -0,0 +1,88 @@
+import React, { useState, useRef, FormHTMLAttributes } from "react";
+import "./Create_Task.css";
+
+const CreateTask: React.FC = ({ toggleCreateUser, handleRender }) => {
+  const initialState = {
+    user_id: "a3f9d8d3-39b3-4b65-bff0-12a0e6b7c5c3",
+    due_date: "",
+    status_id: "open",
+    task_priority: "",
+    subject: "",
+    description: "",
+  };
+  const [formDetails, setFormDetails] = useState(initialState);
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await fetch("http://localhost:8080/api/v1/tasks", {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(formDetails),
+      });
+      handleRender();
+      toggleCreateUser();
+      setFormDetails(initialState);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <form className="form">
+        <label>
+          {" "}
+          Due Date
+          <input
+            value={formDetails.due_date}
+            type="datetime-local"
+            onChange={(e) =>
+              setFormDetails({ ...formDetails, due_date: e.target.value })
+            }
+          />
+        </label>
+        <label>
+          Task priority
+          <select
+            name="Task_priority"
+            onChange={(e) =>
+              setFormDetails({ ...formDetails, task_priority: e.target.value })
+            }
+          >
+            <option value="">Select task priority</option>
+            <option value="high">high</option>
+            <option value="normal">normal</option>
+            <option value="low">low</option>
+          </select>
+        </label>
+        <label>
+          Subject
+          <input
+            value={formDetails.subject}
+            name="subject"
+            onChange={(e) =>
+              setFormDetails({ ...formDetails, subject: e.target.value })
+            }
+          />
+        </label>
+        <label>
+          Description
+          <input
+            name="description"
+            value={formDetails.description}
+            onChange={(e) =>
+              setFormDetails({ ...formDetails, description: e.target.value })
+            }
+          />
+        </label>
+        <button onClick={handleFormSubmit} className="btn">
+          Create task
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default CreateTask;
