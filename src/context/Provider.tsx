9ded73184e0c5f65681886799e0a60baab9e331d
irungundigirigi <irungundigirigi@gmail.com>
2024-10-29 14:31:56 +0300

    centralise app state

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -5,40 +5,40 @@ import { Task } from "../types/Task";
 const AppContext = createContext<any>({});
 
 const AppProvider = ({ children }) => {
-  const [user_data, c_setData] = useState<Partial<User>>({});
-  const [c_selectedDate, c_setSelectedDate] = useState<Date>(new Date());
-  const [c_render, c_setRender] = useState<boolean>(true);
-  const [c_createTask, c_setUserCreate] = useState<boolean>(true);
+  const [user_data, setData] = useState<Partial<User>>({});
+  const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+  const [render, setRender] = useState<boolean>(true);
+  const [createTask, setUserCreate] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(
         "http://localhost:8080/api/v1/users/a3f9d8d3-39b3-4b65-bff0-12a0e6b7c5c3"
       );
-      c_setData(await response.json());
+      setData(await response.json());
     };
     fetchData();
-  }, [c_render]);
+  }, [render]);
 
-  const c_handleDateChange = async (date: Date) => {
+  const handleDateChange = async (date: Date) => {
     try {
-      await c_setSelectedDate(date);
+      await setSelectedDate(date);
     } catch (error) {
       console.log(error);
     }
   };
 
-  const c_handleRender = () => {
-    c_setRender(!c_render);
+  const handleRender = () => {
+    setRender(!render);
   };
 
-  const c_toggleUserCreate = () => {
-    c_setUserCreate(!c_createTask);
+  const toggleUserCreate = () => {
+    setUserCreate(!createTask);
   };
 
   /** Adjust date to local time before converting it to ISO string */
   const adjustedDate: Date = new Date(
-    c_selectedDate.getTime() - c_selectedDate.getTimezoneOffset() * 60000
+    selectedDate.getTime() - selectedDate.getTimezoneOffset() * 60000
   );
 
   const adjustedDateStr: string = adjustedDate.toISOString().split("T")[0];
@@ -53,14 +53,14 @@ const AppProvider = ({ children }) => {
     <AppContext.Provider
       value={{
         user_data,
-        c_setRender,
-        c_selectedDate,
-        c_createTask,
-        c_setUserCreate,
-        c_setSelectedDate,
-        c_handleDateChange,
-        c_handleRender,
-        c_toggleUserCreate,
+        setRender,
+        selectedDate,
+        createTask,
+        setUserCreate,
+        setSelectedDate,
+        handleDateChange,
+        handleRender,
+        toggleUserCreate,
         todaysTasks,
         adjustedDate,
       }}
