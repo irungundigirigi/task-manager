5c772eca57dd770f523a771db01a7247183de5d2
irungundigirigi <irungundigirigi@gmail.com>
2024-10-30 10:53:04 +0300

    add util function: getTodaysTasks

--- src/util/util.tsx
+++ src/util/util.tsx
@@ -1,6 +1,6 @@
 import { Task } from "../types/Task";
 
-export function calculateRemainingHours(task: Task): string {
+const getRemainingTime = (task: Task): string => {
   const now = new Date();
   const dueDate = new Date(task.due_date);
 
@@ -13,4 +13,19 @@ export function calculateRemainingHours(task: Task): string {
   const hours = differenceInHours % 24;
 
   return `${days} days ${hours} hours`;
-}
+};
+
+const getTodaysTasks = (tasks: Task[] | undefined, selectedDate: Date) => {
+  const eat_Date: Date = new Date(
+    selectedDate.getTime() - selectedDate.getTimezoneOffset() * 60000
+  );
+  const dateOnlyT: string = eat_Date.toISOString().split("T")[0];
+  const daysTasks = tasks?.filter((task: Task) => {
+    const dueDate = task.due_date.split("T")[0];
+    return dueDate === dateOnlyT;
+  });
+
+  return daysTasks;
+};
+
+export { getTodaysTasks, getRemainingTime };
